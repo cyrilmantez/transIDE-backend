df833ea9510a39476b76b60a24493959b328b540
Your name <Your email>
2023-12-13 11:58:48 +0100

    modif model Patient avec sous documents et réajustement des routes Patient

--- routes/patients.js
+++ routes/patients.js
@@ -23,22 +23,33 @@ router.post('/addPatient', (req,res) => {
         yearOfBirthday : req.body.dateOfBirthday,
         address: req.body.address,
         infosAddress : req.body.infosAddress,
-        treatment:{
+        homePhone : req.body.homePhone,
+        mobile: req.body.mobile, 
+        treatments:[{
             state: false,
-            date: req.body.date,
+            date: req.body.treatmentDate,
             actions: [req.body.actions],
-            nurse: req.body.username
-        },
-        phoneNumbers: {
-            home: req.body.homePhone,
-            mobile : req.body.mobilePhone,
-        },
-        inCaseOfEmergency: {
-            identity: req.body.identity,
-            phoneNumber: req.body.phoneNumber,
-        }
-        
+            nurse: req.body.username,
+            documentsOfTreatment: [{
+                creationDate: req.body.creationDateOfDocumentsOfTreatment,
+                urls: [req.body.urlsOfDocumentsOfTreatment]
+            }],
+        }],
+        documents: [{
+            creationDate: req.body.creationDateOfDocument,
+            url: req.body.urlOfDocument
+        }],    
+        transmissions: [{
+                date: req.body.transmissionDate,
+                nurse : req.body.username,
+                info : req.body.info,
+                urlDocument: req.body.urlDocument,    
+        }],
+        disponibility: true,
+        ICEIdentity: req.body.ICEIdentity,
+        ICEPhoneNumber: req.body.ICEPhoneNumber,   
     });
+
     newPatient.save().then(data => {
         res.json ({result : true, patientCreate : data})
     })
@@ -61,8 +72,8 @@ router.put('/updatePatientById', (req, res)=> {
 
 ////////////// ajouter des soins à un patient :
 
-router.put('addTreatment', (req, res) => {
-    Patient.updateOne({_id: req.body._id}, {treatment : [...treatment, ...req.body]}).then(data => {
+router.put('/addTreatment', (req, res) => {
+    Patient.updateOne({_id: req.body._id}, {treatments : [...treatments, req.body]}).then(data => {
         res.json({result: true})
     })
 })
@@ -84,9 +95,9 @@ router.delete('/deletePatient/:_id', (req, res)=> {
 
 ///////////// recupération de tous les patients à voir pour le jour :
 
-router.get('/allPatients/:dateOfToday', (req, res) => {
-    Patient.find().then(data => {
-         const allPatientsToSee =  data.filter(patient => patient.treatment.date === req.params.dateOfToday);
+router.get('/allPatients', (req, res) => {
+    Patient.find({officeToken: req.body.officeToken}).then(data => {
+         const allPatientsToSee =  data.filter(patient => patient.treatments.date === req.body.dateOfToday);
          res.json({result: true, patientsToSee: allPatientsToSee})
     })
 
